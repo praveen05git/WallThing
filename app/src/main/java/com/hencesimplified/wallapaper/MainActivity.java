d0d4274e9360f47a8638037985d4adfdaa234320
praveen <praveen051997@gmail.com>
2019-11-01 11:25:04 +0530

    Final

--- app/src/main/java/com/hencesimplified/wallapaper/MainActivity.java
+++ app/src/main/java/com/hencesimplified/wallapaper/MainActivity.java
@@ -5,7 +5,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -135,5 +138,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.opt_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int opt_id=item.getItemId();
+
+        switch (opt_id)
+        {
+            case R.id.opt_remove:
+                AlertDialog alert_dia1=new AlertDialog.Builder(MainActivity.this).create();
+                alert_dia1.setTitle("Remove Ads!");
+                alert_dia1.setMessage("Download the Wall Thing Pro to enjoy Ad-free service!! Download Now?");
+
+                alert_dia1.setButton(AlertDialog.BUTTON_POSITIVE, "Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.jotterpro.notes&hl=en_IN")));
+                    }
+                });
+                alert_dia1.setButton(AlertDialog.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                alert_dia1.show();
+
+                return true;
+
+            case R.id.opt_rate:
+                Intent playStore = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.jotter.notes"));
+                startActivity(playStore);
+                return true;
+
+            case R.id.opt_more:
+                try {
+                    //replace &quot;Unified+Apps&quot; with your developer name https://play.google.com/store/apps/developer?id=Hence+Simplified&hl=en
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pub:Hence Simplified")));
+                }
+                catch (android.content.ActivityNotFoundException e) {
+                    //replace &quot;Unified+Apps&quot; with your developer name
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/search?q=pub:Hence Simplified")));
+                }
+                return true;
+
+            case R.id.opt_about:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
